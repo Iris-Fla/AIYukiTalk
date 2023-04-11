9ab10fcb22ba13f28974d134d3a133eb5bf02c7b
Iris-Fla <irith.fla@gmail.com>
2023-04-11 15:22:25 +0900

    .env.sample

--- yuki.py
+++ yuki.py
@@ -1,18 +1,15 @@
 import get_env
 import os
-
 import openai
 
 # APIキーの設定
 openai.api_key = (get_env.OpenAIAPI)
 
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "大谷翔平について教えて"},
-    ],
-)
-print(response.choices[0]["message"]["content"].strip())
-
+#
+def usegpt(self):
+    print("a")
+    
+    
+    
+print(usegpt.choices[0]["message"]["content"].strip())
 
