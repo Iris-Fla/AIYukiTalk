c18b0e515d478f3078a04b328502cbb8bf658b3a
Iris-Fla <irith.fla@gmail.com>
2023-04-11 13:17:49 +0900

    update

--- get_env.py
+++ get_env.py
@@ -7,6 +7,4 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-DSN = os.environ.get("DB_NAME")
-USN = os.environ.get("USER_NAME")
-PWD = os.environ.get("PASSWORD")
+OpenAIAPI = os.environ.get("OpenAIApikey")
--- yuki.py
+++ yuki.py
@@ -1,2 +1,18 @@
 import get_env
-print(get_env.DSN)
+import os
+
+import openai
+
+# APIキーの設定
+openai.api_key = (get_env.OpenAIAPI)
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "大谷翔平について教えて"},
+    ],
+)
+print(response.choices[0]["message"]["content"].strip())
+
+
