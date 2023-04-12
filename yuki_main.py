580623422d06d86efd8756bcf47eb9f02dcb3640
Iris-Fla <irith.fla@gmail.com>
2023-04-12 18:59:58 +0900

    環境変数辺りの改善～！

--- yuki_main.py
+++ yuki_main.py
@@ -1,20 +1,72 @@
-import sys
-import get_env
-import openai
-import requests
-import simpleaudio
 import tempfile
 import json
+import sys
+import openai
+import time
+import requests
+import tkinter as tk
+import os
+from dotenv import load_dotenv
+
+import simpleaudio
 import speech_recognition as sr
 
-# APIキーの設定
-BotContent = (get_env.BotContent)
-openai.api_key = (get_env.OpenAIAPI)
+# APIキーの設定(.envにAPIKEY,BotContentをいれてください)
+load_dotenv()
+openai.api_key = os.environ.get("OpenAIApikey")
+BotContent = os.environ.get("BotContent")
+# GUIウィンドウを生成する関数
 
-# ChatGPT(.envにAPIKEY,BotContentをいれてください)
+
+def create_window():
+    window = tk.Tk()
+    window.title("チャットアプリ")
+    window.geometry("400x500")
+    return window
+
+# チャットログを表示するテキストボックスを生成する関数
 
 
-def usegpt(text):
+def create_chat_log_textbox(window):
+    chat_log_textbox = tk.Text(window)
+    chat_log_textbox.place(x=10, y=10, width=380, height=400)
+    return chat_log_textbox
+
+# メッセージを入力するエントリーボックスを生成する関数
+
+
+def create_message_entry(window):
+    message_entry = tk.Entry(window)
+    message_entry.place(x=10, y=420, width=300, height=30)
+    return message_entry
+
+# メッセージを送信するボタンを生成する関数
+
+
+def create_send_button(window, message_entry, chat_log_textbox):
+    def send_message():
+        message = message_entry.get()
+        chat_log_textbox.insert(tk.END, "You: " + message + "\n")
+        message_entry.delete(0, tk.END)
+    send_button = tk.Button(window, text="送信", command=send_message)
+    send_button.place(x=320, y=420, width=70, height=30)
+    return send_button
+
+
+# GUIウィンドウを生成
+window = create_window()
+
+# チャットログを表示するテキストボックスを生成
+chat_log_textbox = create_chat_log_textbox(window)
+
+# メッセージを入力するエントリーボックスを生成
+message_entry = create_message_entry(window)
+
+# メッセージを送信するボタンを生成
+send_button = create_send_button(window, message_entry, chat_log_textbox)
+
+
+def usegpt(text):  # ChatGPT
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -23,13 +75,10 @@ def usegpt(text):
         ],
     )
     gpt_response = response.choices[0]["message"]["content"].strip()
-    # print(gpt_response)
     return gpt_response
-# 動作確認 usegpt("あなたの名前を教えてください。")
 
 
-# VoiceVox(VoiceVoxを起動した状態でないと動きません)
-def Voivo(text):
+def Voivo(text):  # VoiceVox(VoiceVoxを起動した状態でないと動きません)
     speak_text = (("text", text), ("speaker", 16))
     send1 = requests.post(
         f"http://localhost:50021/audio_query", params=speak_text)
@@ -42,21 +91,21 @@ def Voivo(text):
                 f"{tmp}/audi.wav")
             waitsora = sora_voice.play()
             waitsora.wait_done()
-# 動作確認 Voivo("眠いです")
 
 
-def recog():
+def recog():  # SpeechRecognizer
     listener = sr.Recognizer()
     while True:
         try:
             with sr.Microphone() as source:
                 print("お話してください～♪")
                 voice = listener.listen(source)
-                print("考え中です...!")
+                print("...")
                 voice_text = listener.recognize_google(voice, language="ja-JP")
                 return voice_text
         except sr.UnknownValueError:
-            print("聞き取れませんでした...")
+            print("聞き取れませんでした...！")
+            time.sleep(3)
         except sr.RequestError as e:
             print(
                 "Could not request results from Google Speech Recognition service; {0}".format(e))
